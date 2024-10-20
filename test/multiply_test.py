645a700dfb0953cfdad9ae74270e277cdbfd3da7
Melissa9mpenzi <melissampenzi@gmail.com>
2024-10-20 13:48:33 +0300

    First commit

---
+++ test/multiply_test.py
@@ -0,0 +1,14 @@
+from multiply import multiply
+
+
+#def test_multiply():
+    #assert multiply(2,3) == 6
+
+#def test_multiply(a,b):
+    #return 6
+
+def test_multiply_with_1_and_1():
+    assert multiply(1, 1) == 1
+
+def test_multiply_with_2_and_3():
+    assert multiply(2, 3) == 6
